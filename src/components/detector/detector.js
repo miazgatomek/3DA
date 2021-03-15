8836202b9921e2e752f642d0a61a5d9383313313
Tomasz Miazga <tom3k.miazga@gmail.com>
2021-03-15 17:41:25 +0100

    Change page title and rearrange Detector

--- src/components/detector/detector.js
+++ src/components/detector/detector.js
@@ -1,27 +1,31 @@
-import React, {Component} from 'react';
+import React from 'react';
 import './detector.scss'
 import {Canvas} from "react-three-fiber";
 import {OrbitControls} from "@react-three/drei";
 import * as THREE from "three";
 
-class Detector extends Component {
-    render() {
-        return (
-            <React.Fragment>
-                <div className="detector__container">
-                    <Canvas>
-                        <OrbitControls/>
-                        <ambientLight/>
-                        <primitive object={new THREE.AxesHelper(10)} />
-                        <mesh>
-                            <boxBufferGeometry attach="geometry"/>
-                            <meshLambertMaterial attach="material" color="red"/>
-                        </mesh>
-                    </Canvas>
-                </div>
-            </React.Fragment>
-        );
-    }
+const Box = () => {
+    return (
+        <mesh>
+            <boxBufferGeometry attach="geometry"/>
+            <meshLambertMaterial attach="material" color="red"/>
+        </mesh>
+    );
+}
+
+const Detector = () => {
+    return (
+        <React.Fragment>
+            <div className="detector__container">
+                <Canvas>
+                    <OrbitControls/>
+                    <ambientLight/>
+                    <primitive object={new THREE.AxesHelper(10)} />
+                    <Box/>
+                </Canvas>
+            </div>
+        </React.Fragment>
+    );
 }
 
 export default Detector;
