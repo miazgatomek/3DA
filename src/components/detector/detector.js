61a6fec87d2de2621d666d3bf6d2b14795ff6bb8
Tomasz Miazga <tom3k.miazga@gmail.com>
2021-03-16 19:29:37 +0100

    Add props to Box

--- src/components/detector/detector.js
+++ src/components/detector/detector.js
@@ -4,15 +4,27 @@ import {Canvas} from "react-three-fiber";
 import {OrbitControls} from "@react-three/drei";
 import * as THREE from "three";
 
-const Box = () => {
+const Box = (props) => {
     return (
-        <mesh>
-            <boxBufferGeometry attach="geometry"/>
+        <mesh position={[props.x, props.y, props.z]}>
+            <boxBufferGeometry args={[8, 1, 1]}/>
             <meshLambertMaterial attach="material" color="red"/>
         </mesh>
     );
 }
 
+const InnerDonut = () => {
+    return (
+        <React.Fragment>
+            <mesh>
+                <Box x={0} y={3} z={0}/>
+                <Box x={0} y={-3} z={0}/>
+                <Box x={0} y={0} z={-3}/>
+                <Box x={0} y={0} z={3}/>
+            </mesh>
+        </React.Fragment>
+    );
+}
 
 const Detector = () => {
     return (
@@ -22,8 +34,8 @@ const Detector = () => {
                     <OrbitControls/>
                     <ambientLight intensity={0.1}/>
                     <spotLight position={[100, 100, 100]} angle={0.15} penumbra={1}/>
-                    <primitive object={new THREE.AxesHelper(10)} />
-                    <Box/>
+                    <primitive object={new THREE.AxesHelper(1000)} />
+                    <InnerDonut/>
                 </Canvas>
             </div>
         </React.Fragment>
