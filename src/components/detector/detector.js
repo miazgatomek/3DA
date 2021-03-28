940c94ba1d262cb5da07b26534de4308213a1236
Tomasz Miazga <tom3k.miazga@gmail.com>
2021-03-28 16:35:44 +0200

    Improve InnerDonut

--- src/components/detector/detector.js
+++ src/components/detector/detector.js
@@ -1,26 +1,48 @@
-import React from 'react';
+import React, {useMemo, useRef, createRef, componentDidMount, Component} from 'react';
 import './detector.scss'
-import {Canvas} from "react-three-fiber";
+import {Canvas, useFrame} from "react-three-fiber";
 import {OrbitControls} from "@react-three/drei";
 import * as THREE from "three";
 
-const Box = (props) => {
-    return (
-        <mesh position={[props.x, props.y, props.z]}>
-            <boxBufferGeometry args={[8, 1, 1]}/>
-            <meshLambertMaterial attach="material" color="red"/>
-        </mesh>
-    );
+
+export const INNER_RADIUS = 2;
+
+
+export class Box extends Component {
+    constructor() {
+        super();
+        this.meshRef = createRef();
+    }
+
+    componentDidMount() {
+        console.log(this.props);
+        this.meshRef.current.rotation.x = this.props.rotation;
+    }
+
+    render() {
+        return (
+            <mesh ref={this.meshRef} position={[this.props.x, this.props.y, this.props.z]}>
+                <boxBufferGeometry args={[8, 0.55, 0.55]}/>
+                <meshLambertMaterial attach="material" color="lightgreen" transparent={true} opacity={0.5}/>
+            </mesh>
+        );
+    }
 }
 
 const InnerDonut = () => {
+    const mesh = useRef();
+    useFrame(() => (mesh.current.rotation.y += 0.01))
+
+    const degreesToRadians = (degrees) => (Math.PI / 180) * degrees;
+    const getY = (i) => Math.sin(degreesToRadians(i * 20)) * INNER_RADIUS
+    const getZ = (i) => Math.cos(degreesToRadians(i * 20)) * INNER_RADIUS
+
+    const props = Array.from(Array(19), (_, i) => ({x: 0, y: getY(i), z: getZ(i), rotation: degreesToRadians(360 - i * 20)}));
+
     return (
         <React.Fragment>
-            <mesh>
-                <Box x={0} y={3} z={0}/>
-                <Box x={0} y={-3} z={0}/>
-                <Box x={0} y={0} z={-3}/>
-                <Box x={0} y={0} z={3}/>
+            <mesh ref={mesh}>
+                {props.map((prop, i) => <Box {...prop} key={i}/>)}
             </mesh>
         </React.Fragment>
     );
