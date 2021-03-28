21afcbd963db2c2fcdef7bc807aea6f30933e483
Tomasz Miazga <tom3k.miazga@gmail.com>
2021-03-28 17:18:42 +0200

    Add Outer Donut

--- src/components/detector/detector.js
+++ src/components/detector/detector.js
@@ -6,16 +6,22 @@ import * as THREE from "three";
 import {Donut} from "./components/donut";
 
 
-export const INNER_DONUT_COLOR = 'lightgreen'
+export const INNER_DONUT_COLOR = '#4DED30'
 export const INNER_DONUT_RADIUS = 2;
-export const INNER_BOX_WIDTH = 0.55;
-export const INNER_BOX_HEIGHT = 0.8;
+export const INNER_BOX_WIDTH = 0.4;
+export const INNER_BOX_HEIGHT = 1.5;
 export const INNER_BOX_LENGTH = 8;
 
-export const OUTER_DONUT_COLOR = 'red'
-export const OUTER_DONUT_RADIUS = 3;
-export const OUTER_BOX_WIDTH = 0.95;
-export const OUTER_BOX_HEIGHT = 0.55;
+export const MIDDLE_DONUT_COLOR = '#949494'
+export const MIDDLE_DONUT_RADIUS = 3.1;
+export const MIDDLE_BOX_WIDTH = 0.95;
+export const MIDDLE_BOX_HEIGHT = 0.55;
+export const MIDDLE_BOX_LENGTH = 8;
+
+export const OUTER_DONUT_COLOR = '#EE2400'
+export const OUTER_DONUT_RADIUS = 4;
+export const OUTER_BOX_WIDTH = 1.43;
+export const OUTER_BOX_HEIGHT = 0.1;
 export const OUTER_BOX_LENGTH = 8;
 
 const Detector = () => {
@@ -33,6 +39,13 @@ const Detector = () => {
                         width={INNER_BOX_WIDTH}
                         height={INNER_BOX_HEIGHT}
                         length={INNER_BOX_LENGTH}
+                    />
+                    <Donut
+                        color={MIDDLE_DONUT_COLOR}
+                        radius={MIDDLE_DONUT_RADIUS}
+                        width={MIDDLE_BOX_WIDTH}
+                        height={MIDDLE_BOX_HEIGHT}
+                        length={MIDDLE_BOX_LENGTH}
                     />
                     <Donut
                         color={OUTER_DONUT_COLOR}
