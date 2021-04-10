9cfc855380212437d40c4f61d228025546e535ca
Tomasz Miazga <tom3k.miazga@gmail.com>
2021-04-10 14:49:34 +0200

    Add basic buttons logic

--- src/components/detector/detector.js
+++ src/components/detector/detector.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './detector.scss'
 import {Canvas} from "react-three-fiber";
 import {OrbitControls} from "@react-three/drei";
@@ -8,9 +8,31 @@ import TrackEvent from "./components/track-event";
 
 
 const Detector = () => {
+    const [eventIndex, setEventIndex] = useState(1);
+    const _setEventIndex = (index) => {
+        if (index >= 1 && index <= 32) {
+            setEventIndex(index);
+        }
+    };
+
     return (
         <React.Fragment>
             <div className="detector__container">
+                <div className="detector__buttons-row">
+                    <button
+                        className="detector__button detector__button__previous"
+                        onClick={() => _setEventIndex(eventIndex - 1)}
+                    >
+                        Previous Event
+                    </button>
+                    <span className="detector__event-index">{eventIndex} / 32</span>
+                    <button
+                        className="detector__button detector__button__next"
+                        onClick={() => _setEventIndex(eventIndex + 1)}
+                    >
+                        Next Event
+                    </button>
+                </div>
                 <Canvas camera={{position: [10, 6, 10]}}>
                     <OrbitControls/>
                     <ambientLight intensity={0.1}/>
