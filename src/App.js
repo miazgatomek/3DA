2bc775cffa9561743576a0443cbfb3ba414ffe85
Tomasz Miazga <tom3k.miazga@gmail.com>
2021-05-05 19:30:58 +0200

    Add About page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import * as THREE from "three";
 import TrackEvent from "./components/track-event/track-event";
 import LineEvent from "./components/line-event/line-event";
 import React, {useState} from "react";
+import {ABOUT_TEXT} from "./utils/about-text";
 
 
 const NUM_OF_EVENTS = 12;
@@ -27,56 +28,81 @@ function App() {
         setShowPoints(!showPoints);
     }
 
+    const [currentScreen, setCurrentScreen] = useState('detector');
+
+    const handleDetectorClick = () => {
+        setCurrentScreen('detector');
+    }
+
+    const handleAboutClick = () => {
+        setCurrentScreen('about');
+    }
+
   return (
     <div className="App">
-        <Header/>
+        <Header
+            onDetectorClick={handleDetectorClick}
+            onAboutClick={handleAboutClick}
+        />
 
-        <div className="canvas__buttons-row">
-            <div className="canvas__button-group">
-                <button
-                    className="canvas__button"
-                    onClick={() => _setEventIndex(eventIndex - 1)}
-                >
-                    Previous Event
-                </button>
-
-                <button
-                    className="canvas__button"
-                    onClick={() => _setEventIndex(eventIndex + 1)}
-                >
-                    Next Event
-                </button>
-
-                <div className="canvas__event-index">
-                    <span>{eventIndex} / {NUM_OF_EVENTS}</span>
-                </div>
-            </div>
-
-            <div className="canvas__button-group">
-                <div className="canvas__event-index">
-                    <span>{showPoints ? 'Points' : 'Lines'}</span>
-                </div>
-
-                <button
-                    className="canvas__button"
-                    onClick={() => _setShowPoints()}
-                >
-                    Switch between lines/points
-                </button>
-            </div>
-        </div>
-
+        {currentScreen === 'about' &&
         <div className="canvas__container">
-            <Canvas camera={{position: [10, 6, 10]}}>
-                <OrbitControls/>
-                <ambientLight intensity={0.1}/>
-                <spotLight position={[100, 100, 100]} angle={0.15} penumbra={1}/>
-                {/*<primitive object={new THREE.AxesHelper(1000)} />*/}
-                {showPoints && <TrackEvent eventIndex={eventIndex}/>}
-                {!showPoints && <LineEvent eventIndex={eventIndex}/>}
-                <Detector/>
-            </Canvas>
+            <div className="canvas__header-container">
+                <span className="canvas__about-text">{ABOUT_TEXT}</span>
+            </div>
         </div>
+        }
+
+        {currentScreen === 'detector' &&
+        (
+        <div className="canvas__container">
+            <div className="canvas__buttons-row">
+                <div className="canvas__button-group">
+                    <button
+                        className="canvas__button"
+                        onClick={() => _setEventIndex(eventIndex - 1)}
+                    >
+                        Previous Event
+                    </button>
+
+                    <button
+                        className="canvas__button"
+                        onClick={() => _setEventIndex(eventIndex + 1)}
+                    >
+                        Next Event
+                    </button>
+
+                    <div className="canvas__event-index">
+                        <span>{eventIndex} / {NUM_OF_EVENTS}</span>
+                    </div>
+                </div>
+
+                <div className="canvas__button-group">
+                    <div className="canvas__event-index">
+                        <span>{showPoints ? 'Points' : 'Lines'}</span>
+                    </div>
+
+                    <button
+                        className="canvas__button"
+                        onClick={() => _setShowPoints()}
+                    >
+                        Switch between lines/points
+                    </button>
+                </div>
+            </div>
+            <div className="canvas__detector-container">
+                <Canvas camera={{position: [10, 6, 10]}}>
+                    <OrbitControls/>
+                    <ambientLight intensity={0.1}/>
+                    <spotLight position={[100, 100, 100]} angle={0.15} penumbra={1}/>
+                    {/*<primitive object={new THREE.AxesHelper(1000)} />*/}
+                    {showPoints && <TrackEvent eventIndex={eventIndex}/>}
+                    {!showPoints && <LineEvent eventIndex={eventIndex}/>}
+                    <Detector/>
+                </Canvas>
+            </div>
+        </div>
+        )}
     </div>
   );
 }
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,13 +3,31 @@ import './header.scss'
 
 
 class Header extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    handleDetector() {
+        this.props.onDetectorClick();
+    }
+
+    handleAbout() {
+        this.props.onAboutClick();
+    }
+
     render() {
         return (
             <React.Fragment>
                 <div className="header__container">
                     <h2 className="header__title">ALICE</h2>
-                    <span className="header__element">Detector</span>
-                    <span className="header__element">About</span>
+                    <a
+                        onClick={this.handleDetector.bind(this)}
+                        className="header__element"
+                    >Detector</a>
+                    <a
+                        onClick={this.handleAbout.bind(this)}
+                        className="header__element"
+                    >About</a>
                 </div>
             </React.Fragment>
         );
