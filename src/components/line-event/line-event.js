9ea011c913cc62c7877b8a1177d3b98b120a0a3b
Tomasz Miazga <tom3k.miazga@gmail.com>
2021-05-21 16:03:06 +0200

    Clear code and change styling

--- src/components/line-event/line-event.js
+++ src/components/line-event/line-event.js
@@ -12,14 +12,10 @@ import event8 from '../../utils/events/event3055_run226466.json';
 import event9 from '../../utils/events/event3205_run122374.json';
 import event10 from '../../utils/events/event3490_run226466.json';
 import event11 from '../../utils/events/event4047_run226466.json';
-import {useFrame} from "react-three-fiber";
-import useDynamicRefs from "use-dynamic-refs";
 
 const EVENTS = [event0, event1, event2, event3, event4, event5, event6, event7, event8, event9, event10, event11];
 
 const LineEvent = (props) => {
-    const [getRef, setRef] = useDynamicRefs();
-
     const tracksData = EVENTS[props.eventIndex - 1].fTracks;
     const tracksIds = Array.from(Array(tracksData.length).keys()).map(element => element.toString());
 
@@ -33,30 +29,12 @@ const LineEvent = (props) => {
        })
     });
 
-    let position = 0;
-
-    useFrame(() => {
-        if (position < tracksData[0].fPolyX.length) {
-            if (position > 0) {
-                // console.log(position)
-                // tracksIds.forEach((id, i) => {
-                //     const ref = getRef(id);
-                //     ref.current.geometry.setDrawRange(0, position);
-                // })
-            }
-            // let ref = getRef(tracksIds[0]);
-            // console.log(ref.current.geometry);
-            position++;
-        }
-    });
-
     return (
         <React.Fragment>
             <mesh>
                 {
-                    tracksIds.map((id, i) => (
+                    tracksIds.map((_, i) => (
                         <Line
-                            ref={setRef(id)}
                             points={points[i]}
                             key={i}
                             color={'yellow'}
