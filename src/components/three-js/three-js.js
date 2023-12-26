01ec99502323bd0cb9edfb42ba22fcbe4feffce6
Tomasz Miazga <tom3k.miazga@gmail.com>
2023-12-26 15:36:07 +0100

    Add GLTF loader

--- src/components/three-js/three-js.js
+++ src/components/three-js/three-js.js
@@ -1,75 +1,30 @@
-import { OrbitControls } from "@react-three/drei";
-import Detector from "components/three-js/components/detector/detector";
-import LineEvent from "components/three-js/components/line-event/line-event";
-import TrackEvent from "components/three-js/components/track-event/track-event";
-import React, { useState } from "react";
 import { Canvas } from "react-three-fiber";
+import { useLoader } from "@react-three/fiber";
+import { Environment, OrbitControls } from "@react-three/drei";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import React, { Suspense } from "react";
 import './three-js.scss'
 
-const NUM_OF_EVENTS = 12;
+const Model = () => {
+    const gltf = useLoader(GLTFLoader, "Alice-CERN/models/Poimandres.gltf");
+    console.log(gltf)
+    return (
+      <>
+        <primitive object={gltf.scene} />
+      </>
+    );
+  };
 
 export default function ThreeJS() {
-    const [eventIndex, setEventIndex] = useState(1);
-    const _setEventIndex = (index) => {
-        if (index >= 1 && index <= NUM_OF_EVENTS) {
-            setEventIndex(index);
-        } else {
-            setEventIndex(index === NUM_OF_EVENTS + 1 ? 1 : NUM_OF_EVENTS);
-        }
-    };
-
-    const [showTracks, setShowTracks] = useState(true);
-    const _setShowTracks = () => {
-        setShowTracks(!showTracks);
-    }
-
-    // const [currentScreen, setCurrentScreen] = useState('detector');
-
     return (
         <div className="canvas__container">
-            <div className="canvas__buttons-row">
-                <div className="canvas__button-group">
-                    <button
-                        className="canvas__button"
-                        onClick={() => _setEventIndex(eventIndex - 1)}
-                    >
-                        Previous Event
-                    </button>
-
-                    <button
-                        className="canvas__button"
-                        onClick={() => _setEventIndex(eventIndex + 1)}
-                    >
-                        Next Event
-                    </button>
-
-                    <div className="canvas__event-index">
-                        <span>{eventIndex} / {NUM_OF_EVENTS}</span>
-                    </div>
-                </div>
-
-                <div className="canvas__button-group">
-                    <div className="canvas__event-index">
-                        <span>{showTracks ? 'Tracks' : 'Lines'}</span>
-                    </div>
-
-                    <button
-                        className="canvas__button"
-                        onClick={() => _setShowTracks()}
-                    >
-                        Switch between lines/tracks
-                    </button>
-                </div>
-            </div>
             <div className="canvas__detector-container">
-                <Canvas camera={{position: [10, 6, 10]}}>
-                    <OrbitControls/>
-                    <ambientLight intensity={0.1}/>
-                    <spotLight position={[100, 100, 100]} angle={0.15} penumbra={1}/>
-                    {/*<primitive object={new THREE.AxesHelper(1000)} />*/}
-                    {showTracks && <TrackEvent eventIndex={eventIndex}/>}
-                    {!showTracks && <LineEvent eventIndex={eventIndex}/>}
-                    <Detector/>
+                <Canvas>
+                    <Suspense fallback={null}>
+                        <OrbitControls/>
+                        <Environment preset="sunset" background />
+                        <Model/>
+                    </Suspense>
                 </Canvas>
             </div>
         </div>
--- src/core/components/header/header.js
+++ src/core/components/header/header.js
@@ -6,14 +6,14 @@ import React from 'react'
 export function Header() {
     return (
         <div className="header__container">
-            <h2 className="header__title">ALICE</h2>
+            <h2 className="header__title">3DA</h2>
             <Link className="header__element" to="three-js">
                 ThreeJS
             </Link>
             <Link
                 to="about-alice"
                 className="header__element"
-            >About ALICE</Link>
+            >About</Link>
         </div>
     )
 }
