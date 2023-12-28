398583f065076863ab30b04e255e9ed96210bed1
Tomasz Miazga <tom3k.miazga@gmail.com>
2023-12-28 11:24:54 +0100

    Upgrade to React 18

--- src/components/three-js/three-js.js
+++ src/components/three-js/three-js.js
@@ -1,4 +1,4 @@
-import { Canvas } from "react-three-fiber";
+import { Canvas } from "@react-three/fiber";
 import { useLoader } from "@react-three/fiber";
 import { Environment, OrbitControls } from "@react-three/drei";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
