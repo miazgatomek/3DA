9f6465b09bd1bbe2cb553e37895e2e9d4f181639
Tomasz Miazga <tom3k.miazga@gmail.com>
2023-12-28 11:28:59 +0100

    Updates

---
+++ static/js/594.6e3ddee4.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkalice=self.webpackChunkalice||[]).push([[594],{594:function(a,e,c){c.r(e),c.d(e,{default:function(){return s}});c(791);const i="Web page dedicated for 3D graphics libraries analysis.";var n=c(184);function s(){return(0,n.jsx)("div",{className:"about-alice__container",children:(0,n.jsx)("span",{className:"about-alice__about-text",children:i})})}}}]);
+//# sourceMappingURL=594.6e3ddee4.chunk.js.map
