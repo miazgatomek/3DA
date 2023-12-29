8e7936ece58ee04891da2901bcd36a0c4d2dcdfc
Tomasz Miazga <tom3k.miazga@gmail.com>
2023-12-29 12:13:59 +0100

    Styling

--- src/components/three-js/three-js.js
+++ src/components/three-js/three-js.js
@@ -1,13 +1,15 @@
 import { Canvas } from "@react-three/fiber";
 import { useLoader } from "@react-three/fiber";
-import { Environment, OrbitControls } from "@react-three/drei";
+import { OrbitControls } from "@react-three/drei";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import React, { Suspense } from "react";
 import './three-js.scss'
 
 const Model = () => {
+    console.time('Model Load');
     const gltf = useLoader(GLTFLoader, "models/Poimandres.gltf");
     console.log(gltf)
+    console.timeEnd('Model Load');
     return (
       <>
         <primitive object={gltf.scene} />
@@ -19,10 +21,9 @@ export default function ThreeJS() {
     return (
         <div className="canvas__container">
             <div className="canvas__detector-container">
-                <Canvas>
+                <Canvas style={{ background: "lightblue" }}>
                     <Suspense fallback={null}>
                         <OrbitControls/>
-                        <Environment preset="sunset" background />
                         <Model/>
                     </Suspense>
                 </Canvas>
