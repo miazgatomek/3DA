a3969bf49a3afbf8dcf537dc04f597123fa48b4a
Tomasz Miazga <tom3k.miazga@gmail.com>
2024-01-07 00:08:23 +0100

    Add AFrame

--- src/components/model-scene/model-scene.js
+++ src/components/model-scene/model-scene.js
@@ -1,32 +1,20 @@
-import { Canvas } from "@react-three/fiber";
-import { useLoader } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-import React, { Suspense } from "react";
-import './model-scene.scss'
+import 'aframe';
+import React from 'react';
 
-const Model = () => {
-    console.time('Model Load');
-    const gltf = useLoader(GLTFLoader, "models/scene.gltf");
-    console.timeEnd('Model Load');
-    return (
-      <>
-        <primitive object={gltf.scene} />
-      </>
-    );
-  };
+// two swings to right
+// add 'stats' to a-scene to see advanced stats
 
-export default function ThreeJS() {
-    return (
-        <div className="canvas__container">
-            <div className="canvas__detector-container">
-                <Canvas style={{ background: "lightblue" }}>
-                    <Suspense fallback={null}>
-                        <OrbitControls/>
-                        <Model/>
-                    </Suspense>
-                </Canvas>
-            </div>
-        </div>
-    )
+const ModelScene = () => {
+  return (
+    <div className="canvas__container">
+      <div className="canvas__detector-container">
+        <a-scene>
+          <a-entity gltf-model="url(./models/scene.gltf)" play-all-model-animations position="0 0 10"></a-entity>
+        </a-scene>
+      </div>
+    </div>
+  );
 }
+
+
+export default ModelScene;
