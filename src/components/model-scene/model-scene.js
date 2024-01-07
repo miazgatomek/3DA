26152339fc11c729b9de137773238e159bcddee8
Tomasz Miazga <tom3k.miazga@gmail.com>
2024-01-07 21:32:16 +0100

    Merge branch 'three-js/parcel' into aframe/parcel

--- src/components/model-scene/model-scene.js
+++ src/components/model-scene/model-scene.js
@@ -3,13 +3,14 @@ import React from 'react';
 
 // two swings to right
 // add 'stats' to a-scene to see advanced stats
+const model = require('../../../public/models/scene.gltf');
 
 const ModelScene = () => {
   return (
     <div className="canvas__container">
       <div className="canvas__detector-container">
         <a-scene>
-          <a-entity gltf-model="url(./models/scene.gltf)" play-all-model-animations position="0 0 10"></a-entity>
+          <a-entity gltf-model={model} play-all-model-animations position="0 0 10"></a-entity>
         </a-scene>
       </div>
     </div>
